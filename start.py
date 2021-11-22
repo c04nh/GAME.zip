06cfb17e4595705e00556328d00accd4949153fe
c04nh <w2038@e-mirim.hs.kr>
2021-11-22 18:04:41 +0900

    Button 생성

--- start.py
+++ start.py
@@ -18,8 +18,17 @@ root.geometry('%dx%d+%d+%d' % (w, h, x, y))     # 창 크기, 위치 지정
 root.configure(background='black')  # 배경색 지정
 
 label1 = Label(root, text='GAME.zip', font=('Arial', 65, 'bold'), fg='blue', bg='black')    # 글씨 생성
-label1.pack(pady='70')  # 글씨 배치 (y좌표 외부 패딩 70)
-label1 = Label(root, text='게임을 선택하세요', font=('Arial', 20), fg='white', bg='black')  # 글씨 생성
-label1.pack()   # 글씨 배치
+label1.place(x=0, y=200, relx=0.5, anchor="s")  # 글씨 배치
+label2 = Label(root, text='게임을 선택하세요', font=('돋움', 25), fg='white', bg='black')  # 글씨 생성
+label2.place(x=0, y=303, relx=0.5, anchor="s")   # 글씨 배치
+
+btn1 = Button(root, text='운석깨기', font=('돋움', 18), fg='red')     # 버튼 생성
+btn2 = Button(root, text='벌레잡기', font=('돋움', 18), fg='red')     # 버튼 생성
+btn3 = Button(root, text='테트리스', font=('돋움', 18), fg='red')     # 버튼 생성
+btn1.place(x=0, y=380, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
+btn2.place(x=0, y=450, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
+btn3.place(x=0, y=520, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
+label3 = Label(root, text='made by eunwon and nahyun', font=('Arial', 15), fg='white', bg='black')  # 글씨 생성
+label3.pack(side="bottom", anchor="e", pady=10, padx=10)    # 글씨 배치
 
 root.mainloop()
