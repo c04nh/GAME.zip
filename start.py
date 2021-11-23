f10a71e2d610db67e0ff7c08c5c6c8a095b9a3da
c04nh <w2038@e-mirim.hs.kr>
2021-11-23 11:24:20 +0900

    운석깨기, 벌레잡기, 스네이크게임 버튼에 연결

--- start.py
+++ start.py
@@ -1,4 +1,22 @@
 from tkinter import *
+import pygame
+import PyShooting
+import catchBug
+import snake
+
+pygame.init()
+
+def shooting():
+    PyShooting.initGame()
+    PyShooting.runGame()
+
+def bug():
+    catchBug.initGame()
+    catchBug.runGame()
+
+def snack():
+    snake.initGame()
+    snake.runGame()
 
 root = Tk()
 root.title("GAME.zip")  # 타이틀 지정
@@ -19,16 +37,20 @@ root.configure(background='black')  # 배경색 지정
 
 label1 = Label(root, text='GAME.zip', font=('Arial', 65, 'bold'), fg='blue', bg='black')    # 글씨 생성
 label1.place(x=0, y=200, relx=0.5, anchor="s")  # 글씨 배치
-label2 = Label(root, text='게임을 선택하세요', font=('돋움', 25), fg='white', bg='black')  # 글씨 생성
-label2.place(x=0, y=303, relx=0.5, anchor="s")   # 글씨 배치
+label2 = Label(root, text='게임을 선택하세요', font=('돋움', 22), fg='white', bg='black')  # 글씨 생성
+label2.place(x=0, y=280, relx=0.5, anchor="s")   # 글씨 배치
 
-btn1 = Button(root, text='운석깨기', font=('돋움', 18), fg='red')     # 버튼 생성
-btn2 = Button(root, text='벌레잡기', font=('돋움', 18), fg='red')     # 버튼 생성
+btn1 = Button(root, text='운석 깨기', font=('돋움', 18), fg='red', command=shooting)     # 버튼 생성
+btn2 = Button(root, text='벌레 잡기', font=('돋움', 18), fg='red', command=bug)     # 버튼 생성
 btn3 = Button(root, text='테트리스', font=('돋움', 18), fg='red')     # 버튼 생성
-btn1.place(x=0, y=380, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
-btn2.place(x=0, y=450, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
-btn3.place(x=0, y=520, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
+btn4 = Button(root, text='스네이크 게임', font=('돋움', 18), fg='red', command=snack)     # 버튼 생성
+btn1.place(x=0, y=360, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
+btn2.place(x=0, y=430, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
+btn3.place(x=0, y=500, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
+btn4.place(x=0, y=570, relx=0.5, anchor="s", width=200, height=50)  # 버튼 배치
 label3 = Label(root, text='made by eunwon and nahyun', font=('Arial', 15), fg='white', bg='black')  # 글씨 생성
-label3.pack(side="bottom", anchor="e", pady=10, padx=10)    # 글씨 배치
+label3.pack(side="bottom", anchor="e", pady=15, padx=10)    # 글씨 배치
+
+
 
 root.mainloop()
